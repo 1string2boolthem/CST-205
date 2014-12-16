c8b65217919d8bf01112e3e8ff4c0146ab455792
1string2boolthem <sgail@Sevs.local>
2014-12-16 15:18:36 -0800

    removed baks and logs and finalized the "minilab"
    The problem was with the getModifier; it was missing the end quote and
    a semicolon. Works like a charm now!

--- mod7minilab.py
+++ mod7minilab.py
@@ -22,22 +22,22 @@ def mostCommonWord(wordDict):
   
 def getModifier(frequency): #returns the modifying html based on the frequency of a word
   size = 7 + int(frequency/10)
-  modifier = "<span style=\"font-size:" + str(size) + "pt "
+  modifier = "<span style=\"font-size:" + str(size) + "pt; color:"
   if frequency == 1:
-    modifier += "color:LightGreen"
+    modifier += "LightGreen"
   elif frequency < 10:
-    modifier += "color:green>"
+    modifier += "green"
   elif frequency < 30:
-    modifier += "color:purple>"
+    modifier += "blue"
   elif frequency < 50:
-    modifier += "color:blue>"
+    modifier += "DarkBlue"
   elif frequency < 70:
-    modifier += "color:orange; font-weight:bold>"
+    modifier += "purple; font-weight:bold"
   elif frequency == 84:
-    modifier += "color:DarkRed; font-weight:bold>"
+    modifier += "DarkRed; font-weight:bold"
   else:
-    modifier += "color:red; font-weight:bold>"
-  return modifier
+    modifier += "LightRed; font-weight:bold"
+  return str(modifier + "\">")
 #greenEggs reads the eggs.txt file input by the user, creates a dictionary with each unique word as a key
 #and the key's value as the number of times that word occurs in the text. It also prints various
 #information to an html document, as per specifications.
@@ -60,27 +60,35 @@ def greenEggs():
   for key in wordDict:
     printNow(key + ": " + str(wordDict[key]))
   content = "<h1>Green Eggs & Ham!</h1>"
-  content += "Number of words in \"Green Eggs & Ham\": " + str(wordCount(wordDict)) + "<br />"
-  content += "Number of unique words in \"Green Eggs & Ham\": " + str(len(wordDict)) + "<br />"
-  content += "The most common word in \"Green Eggs & Ham\": " + mostCommonWord(wordDict) + "<br />"
-  content += "Now, for a visual representation: <br /><br />"
+  content += "Number of words in <b>Green Eggs & Ham</b>: " + str(wordCount(wordDict)) + "<br />"
+  content += "Number of unique words in <b>Green Eggs & Ham</b>: " + str(len(wordDict)) + "<br />"
+  content += "The most common word in <b>Green Eggs & Ham</b>: " + mostCommonWord(wordDict) + "<br />"
+  content += "The number of times each word appears in the story below affects its color and size.<br />"
+  content += "LEGEND: "
+  content += "<span style=\"font-size:7pt; color:LightGreen\">1 time </span>"
+  content += "<span style=\"font-size:8pt; color:green\">2-10 times </span>"
+  content += "<span style=\"font-size:9pt; color:blue\">11-30 times </span>"
+  content += "<span style=\"font-size:10pt; color:DarkBlue\">31-50 times </span>"
+  content += "<span style=\"font-size:11pt; color:purple; font-weight:bold\">51-70 times </span>"
+  content += "<span style=\"font-size:12pt; color:red; font-weight:bold\">71-80 times </span>"
+  content += "<span style=\"font-size:13pt; color:DarkRed; font-weight:bold\">84 times </span><br /><br />"
+
   data = fileData
-  data = data.replace('\n', "<br>")
+  data = data.replace('\n', " <br> ")
   data = data.replace('-', ' ')
   data = re.sub(' +', ' ', data)
-  words = data.replace("<br>", " <br> ")
-  words = words.split(' ')
-  wordList = []
+  words = data.split(' ')
   data = ""
   for word in range(len(words)):
-    if words[word] != "<br>" and words[word] != '':
+    if words[word] == "<br>":
+      data += "<br>"
+    elif words[word] != '':
       frequency = wordDict[words[word].lower()]
       modifier = getModifier(frequency)
       data += modifier + words[word] + "</span> "
-    else:
-      data += "<br>"
   content += data
   makePage(content)
+  printNow(data)
 
 #makePage writes content to a user-supplied html page.
 def makePage(content):
@@ -90,3 +98,5 @@ def makePage(content):
   data = "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transition//EN" "http://www.w3.org/TR/html4/loose.dtd\"><html><head><title>MiniLab</title></head><body>" + content + "</body></html>"
   file.write(data)
   file.close()
+
+greenEggs()
