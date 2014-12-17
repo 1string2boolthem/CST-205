b4d37db243dcde21e97732f1dadf87506a6c913c
1string2boolthem <crendall@csumb.edu>
2014-12-16 17:48:22 -0800

    Added onto lab 16.

--- mod7lab16.py
+++ mod7lab16.py
@@ -11,6 +11,7 @@ def makePage(content):
   data = "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transition//EN" "http://www.w3.org/TR/html4/loose.dtd\"><html><head><title>Lab 16 - URLs and HTML</title></head><body>" + content + "</body></html>"
   file.write(data)
   file.close()
+
 #createRedditCats pulls information from Reddit, creates a list of post titles and links, then creates
 #content from those titles and links. It then calls makePage(content) to write the content to an HTML file.
 def createRedditCats():
@@ -23,10 +24,18 @@ def createRedditCats():
     block = blocks[i]
     title = block.split(">")[1].split("<")[0]
     url = block.split("href=\"")[1].split("\"")[0]
+    commentsUrl = "https://www.reddit.com/r/catpictures/comments" + block.split("https://www.reddit.com/r/catpictures/comments")[1].split("\"")[0]
+    numberOfComments = block.split("<a href=\"https://www.reddit.com/r/catpictures/comments")[1].split(">")[1].split("<")[0]
+    if numberOfComments.find(" ") != -1:
+      numberOfComments = str(numberOfComments.split(" ")[0])
+    else:
+      numberOfComments = "0"
     if ".jpg" in url:
-      posts.append(title + "|||" + url)
+      posts.append(title + "|||" + url + "|||" + numberOfComments + "|||" + commentsUrl)
   content = "<H1>Front Page of <a href=\"https://www.reddit.com/r/catpictures\">/r/catpictures</a></H1>"
   for i in range(0, len(posts)):
     content += "<h2>" + posts[i].split("|||")[0] + "</h2>"
-    content += "<img src=\"" + posts[i].split("|||")[1] + "\" style=\"width:500px; height:500px;\">"
+    content += "<img src=\"" + posts[i].split("|||")[1] + "\" style=\"width:500px; height:500px;\"> <br />"
+    content += "<b>Number of Comments: <a href=\"" + posts[i].split("|||")[3] + "\">" + posts[i].split("|||")[2] + "</a> <br /></b>"
   makePage(content)
+createRedditCats()
